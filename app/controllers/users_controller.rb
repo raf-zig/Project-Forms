0a8ee7a44ada0722bd6dec9f8afa8ecbaac37df1
raf-zig <ziravin@yandex.ru>
2023-06-30 12:34:11 +0500

    Add a edit update methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,22 @@ class UsersController < ApplicationController
     end
   end
 
-  private  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  private
+
   def user_params
     params.require(:user).permit(:username,:email,:password)
   end
